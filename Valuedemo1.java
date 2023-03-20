20004e3e601f3aba657437bcdd4d0d3f8debd150
William <willi2022public@gmail.com>
2023-03-20 21:35:20 +0100

    update am 20.03.2023

---
+++ Valuedemo1.java
@@ -0,0 +1,51 @@
+
+
+public class Valuedemo1{
+	public static void main(String[] args){
+		
+		//ganze Zahl	
+		System.out.println(777);
+		System.out.println(-3.6);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		System.out.println("null");
+		
+		System.out.println("swdfdffd");
+		System.out.println('e');
+		
+		// Funktion von "\t"
+		// er stellt die Laenge der "string" zum 8 oder 8-Fach zaehrige zahl
+		//d.h. die luecke, die er erfuellen will, entspricht minimal 1, maximal 8
+		
+		
+		System.out.println("abc"+"\t"+2);
+		System.out.println("12345678"+"\t");
+		System.out.println("23ac"+"\t"+23);
+		System.out.println("name"+"\t"+ "age");
+		System.out.println("tom"+"\t"+"99");
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
