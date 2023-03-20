a2ddf7752ce332609258ed6851b19551f8f2a506
William <willi2022public@gmail.com>
2023-03-20 21:58:31 +0100

    littel exercise 20.03.2023

---
+++ teacherinfo_exe.java
@@ -0,0 +1,42 @@
+
+
+public class teacherinfo_exe{
+	
+	public static void main (String[] args){
+		
+		String name = "TT2";
+		
+		int age = 18;
+		
+		char gender = 'm';//geschlecht
+		
+		double height = 172.3;
+		
+		boolean flag = true;// ob man single ist
+		
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(gender);
+		System.out.println(height);
+		System.out.println(flag);
+		
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
