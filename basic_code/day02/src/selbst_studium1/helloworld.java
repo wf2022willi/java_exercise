22e49aab9d9e0b3ee59d5eeda46dc850ac2853e6
William <willi2022public@gmail.com>
2023-03-22 23:30:18 +0100

    first try

---
+++ basic_code/day02/src/selbst_studium1/helloworld.java
@@ -0,0 +1,12 @@
+package selbst_studium1;
+
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
+// project -> module -> package -> class
+
+
