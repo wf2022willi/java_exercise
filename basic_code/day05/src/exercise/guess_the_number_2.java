9b7f98e1b491644aac7224d0f2e593dafe52f3d0
William <willi2022public@gmail.com>
2023-04-06 21:24:38 +0200

    random_number_and_exercise

--- basic_code/day05/src/exercise/guess_the_number_2.java
+++ basic_code/day05/src/exercise/guess_the_number_2.java
@@ -7,10 +7,18 @@ public class guess_the_number_2 {
 
     public static void main(String[] args) {
 
+        // 附加条件：三次猜不中， 直接报答案
+        int guess = 0;
+
         Random r = new Random();
         int number = r.nextInt(100) + 1;
         Scanner sc = new Scanner(System.in);
         while (true) {
+            guess ++;
+            if (guess == 3){
+                System.out.println("right...");
+                break;
+            }
 
             System.out.println("pls enter your guess number");
             int temp = sc.nextInt();
