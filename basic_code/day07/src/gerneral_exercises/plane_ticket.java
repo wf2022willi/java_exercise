2d47958cfceb5106cb00ff081be54941239ea029
William <willi2022public@gmail.com>
2023-04-09 22:01:17 +0200

    plane_ticket_exercises

--- basic_code/day07/src/gerneral_exercises/plane_ticket.java
+++ basic_code/day07/src/gerneral_exercises/plane_ticket.java
@@ -2,6 +2,8 @@ package gerneral_exercises;
 
 import java.util.Scanner;
 
+
+// ctrl + alt + M 快捷键 快速抽取方法
 public class plane_ticket {
     public static void main(String[] args) {
          //键盘录入 机票原价 月份 头等舱 或 经济舱
@@ -51,6 +53,43 @@ public class plane_ticket {
             //计算实际价格
             //计算实际价格
     //打印 ， 返回
+
+
+        if ( 5<= month && month <= 10) {
+            getPreis(ticket, seat, 0.9, 0.85);  // variable : ctrl + p
+
+        } else if (month >= 11 && month <= 12 || month >= 1 && month <= 4) {
+            getPreis(ticket, seat, 0.7, 0.65);
+        } else {
+            System.out.println("illegal number");
+        }
+
+        System.out.println(ticket);
+
     }
+
+    // verbessern die Code
+
+    // which variable do i need ? ticket seat rabatt
+    public static  int getPreis(int ticket, int seat, double rabatt0, double rabatt1) {
+        if (seat == 0) {
+                // first class
+                ticket = (int ) (ticket * rabatt0);
+            } else if (seat == 1) {
+                // eco class
+                ticket = (int ) (ticket * rabatt1);
+
+            } else {
+                System.out.println("illegal number, enter again");
+            }
+        return ticket;
+
+    }
+    // inkl. return? yes!
+
+
+
+
+
 
 }
